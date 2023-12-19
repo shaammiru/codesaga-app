c71bf4239b73cb54b89598cd95cebe133e1c0c5b
Muhammad Syamil Hamami <msyamil.hamami@gmail.com>
2023-12-19 14:29:35 +0700

    Change currency

--- app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -45,9 +45,9 @@ export const columns: ColumnDef<Course>[] = [
     },
     cell: ({ row }) => {
       const price = parseFloat(row.getValue("price") || "0");
-      const formatted = new Intl.NumberFormat("en-US", {
+      const formatted = new Intl.NumberFormat("id-ID", {
         style: "currency",
-        currency: "USD"
+        currency: "IDR"
       }).format(price);
 
       return <div>{formatted}</div>
